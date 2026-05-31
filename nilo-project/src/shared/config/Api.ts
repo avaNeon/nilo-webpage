150ffb3ce939220e180df4778f7b4fd2957b6267
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-05-31 10:47:05 +0800

    refactor(api): reorganize api endpoint config

--- nilo-project/src/shared/config/Api.ts
+++ nilo-project/src/shared/config/Api.ts
@@ -2,65 +2,52 @@ import { ServiceType } from "@/shared/model/ServiceType";
 
 //单服务版本
 const Api = {
-  // Backend System configuration
+  // ——————Backend System configuration——————
   systemConfig: "/system/config",
-  // Account
+  // ——————Account——————
   captcha: "/account/captcha",
   login: "/account/login",
   logout: "/account/logout",
   register: "/account/register",
-  sendEmailCode: "/account/sendEmailCode",
   autoLogin: "/account/autoLogin",
-  getUserRelation: "/account/userRelation",
   sourcePath: "/file/image?sourcePath=",
   tmpSourcePath: "/file/image?tmp=true&sourcePath=",
   loadAllCategories: "/category/categories/all",
-  getSysSetting: "/sysSetting/getSetting",
-  //发布视频
+  getUserState: "/account/state",
+  // ——————视频——————
   preUploadVideo: "/file/videoTag",
   uploadVideo: "/file/video",
   delUploadVideo: "/file/video",
   postVideo: "/creativeCenter/video",
-  saveVideoInteraction: "/creativeCenter/saveVideoInteraction",
-  getVideoByVideoId: "/creativeCenter/getVideoByVideoId",
-  loadCreativeCenterVideoList: "/creativeCenter/video/list",
-  getCreativeCenterVideoCountInfo: "/creativeCenter/getVideoCountInfo",
   uploadImage: "/file/image",
-  //创作中心
+  // ——————创作中心——————
+  // 创作中心 - 主页
+  loadRecentStatisticsInfo: "/statistics/recent",
+  // 创作中心 - 视频管理
   ccLoadVideoList: "/creativeCenter/video/list",
   ccLoadVideoCount: "/creativeCenter/video/count",
-  ccLoadVideoInfoFileUploadList:"/creativeCenter/file",
-  ucLoadComment: "/creativeCenter/loadComment",
-  ucDelComment: "/creativeCenter/delComment",
-  //创作中心 - 评论管理
+  ccLoadVideoInfoFileUploadList: "/creativeCenter/file",
+  // 创作中心 - 评论管理
   ccCommentCount: "/creativeCenter/comment/count",
   ccCommentList: "/creativeCenter/comment",
-  ucLoadDanmaku: "/creativeCenter/loadDanmaku",
-  ucDelDanmaku: "/creativeCenter/delDanmaku",
-  //创作中心 - 弹幕管理
+  // 创作中心 - 弹幕管理
   ccDanmakuCount: "/creativeCenter/danmaku",
   ccDanmakuList: "/creativeCenter/danmaku",
-  //弹幕 - 删除
+  // 删除弹幕
   delDanmaku: "/danmaku/danmaku",
-  ucGetActualTimeStatisticsInfo: "/creativeCenter/getActualTimeStatisticsInfo",
-  getWeekStatisticsInfo: "/creativeCenter/getWeekStatisticsInfo",
-  ucDeleteVideo: "/creativeCenter/deleteVideo",
   ccDeleteVideo: "/creativeCenter/video/",
   ccVideoInteraction: "/creativeCenter/video/interaction/",
+  playCount: "/video",
   //获取视频列表
   getFirstLevelCommentCount: "/comment/count",
   loadRecommendVideo: "/video/recommend",
   loadVideoInfo: "/video/video",
   loadVideoFileList: "/video/file",
   hlsMasterPlaylist: "/file/video/hls",
-  getVideoInfo: "/video/getVideoInfo",
   //评论
   commentAction: "/user/commentAction/action",
   getCommentList: "/comment/comment",
   postComment: "/comment/comment",
-  userDelComment: "/comment/userDelComment",
-  userTopComment: "/comment/topComment",
-  userCancelTopComment: "/comment/cancelTopComment",
   //弹幕
   loadDanmaku: "/danmaku/danmaku",
   postDanmaku: "/danmaku/danmaku",
@@ -70,59 +57,10 @@ const Api = {
   getOnlineCount: "/online/count",
   // 视频点赞，投币，收藏
   userVideoAction: "/user/videoAction/action",
-  // 评论点赞，点踩
-  userCommentAction: "/user/commentAction/action",
-  //播放历史
-  playHisotry: "/history/loadHistory",
-  delHistory: "/history/delHistory",
-  cleanHistory: "/history/cleanHistory",
-  //消息
-  getNoReadCount: "/message/getNoReadCount",
-  loadUserMessage: "/message/loadMessage",
-  delMessage: "/message/delMessage",
-  getNoReadCountGroup: "/message/getNoReadCountGroup",
-  readAll: "/message/readAll",
-  //个人主页
-  uHomeUpdateUserInfo: "/uhome/updateUserInfo",
-  uHomeLoadVideo: "/uhome/loadVideoList",
-  uHomeGetUsesrInfo: "/uhome/getUserInfo",
-  //关注
-  follow: "/follow",
-  //取消关注
-  cancelFollow: "/cancelFollow",
-  //关注列表
-  followList: "/loadFollowList",
-  //粉丝列表
-  uHomeFansList: "/uhome/loadFansList",
-  //视频系列
-  uHomeSeriesLoadVideoSeries: "/uhome/series/loadVideoSeries",
-  //获取系列视频
-  uHomeSeriesLoadAllVideo: "/uhome/series/loadAllVideo",
-  //保存系列
-  uHomeSeriesSaveVideoSeries: "/uhome/series/saveVideoSeries",
-  //修改系列顺序
-  uHomeSeriesChangeVideoSeriesSort: "/uhome/series/changeVideoSeriesSort",
-  //获取系列详情
-  uHomeSeriesGetVideoSeriesDetail: "/uhome/series/getVideoSeriesDetail",
-  //删除系列
-  uHomeSeriesDelVideoSeries: "/uhome/series/delVideoSeries",
-  //保存系列视频
-  uHomeSeriesSaveSeriesVideo: "/uhome/series/saveSeriesVideo",
-  //删除系列视频
-  uHomeSeriesDelSeriesVideo: "/uhome/series/delSeriesVideo",
-  //获取所有列表
-  uHomeSeriesLoadVideoSeriesWithVideo: "/uhome/series/loadVideoSeriesWithVideo",
-  //收藏列表
-  uHomeLoadCollection: "/uhome/loadUserCollection",
-  //设置主题
-  saveTheme: "/uhome/saveTheme",
-  //搜索
-  search: "/video/search",
-  getSearchKeywordTop: "/video/getSearchKeywordTop",
-  //推荐视频
-  getVideoRecommend: "/video/getVideoRecommend",
-  //热门视频
-  hotVideoList: "/video/loadHotVideoList",
+  //关注 / 取消关注
+  follow: "/follow/follow",
+  // ——————用户中心——————
+  getUserDetail: "/uHome/user/",
 };
 
 // 区分不同的微服务
