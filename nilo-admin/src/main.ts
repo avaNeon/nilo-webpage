08dbb67ca26ffca38368107391a017e7f78bdcf5
avaNeon <163699989+avaNeon@users.noreply.github.com>
2026-07-02 10:10:13 +0800

    chore(nilo-admin): add iconfont assets

--- nilo-admin/src/main.ts
+++ nilo-admin/src/main.ts
@@ -4,6 +4,8 @@ import router from "@/app/router";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "@/assets/icon/iconfont.css";
+import "@/assets/icon/iconfont-v2.css";
 import "@/assets/scss/base.scss";
 
 const pinia = createPinia();
